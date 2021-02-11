60427c7dd3b7689bca9f352c3fbc6a915337c737
Bayan Khalil <bayan.khalil92@yahoo.com>
2021-02-11 14:24:20 +0200

    finished prop3

--- app.js
+++ app.js
@@ -53,13 +53,17 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) {
-  //eslint-disable-line
   
- 
+  let firstSum = sum(a,b)[0];
+  let secondSum = sum(firstSum,c)[0];
+  let firstMultiply = multiply(a,b)[0];
+  let secondMultiply = multiply(firstMultiply,c)[0];
+  return [secondSum, secondMultiply, `${a} and ${b} and ${c} sum to ${secondSum}.`,`The product of ${a} and ${b} and ${c} is ${secondMultiply}.`];
+ //eslint-disable-line
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -76,9 +80,12 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 let testArray = [2, 3, 4]; //eslint-disable-line
 
-function sumArray(sumArr) {
-  //eslint-disable-line
-}
+// function sumArray(sumArr) {
+//   //eslint-disable-line
+//   let sumArray1=sum((sumArr[0]),(sumArr[1]));
+//   let sumArray2=sum(sumArray1,(sumArr[2]))
+
+// }
 
 // Here is the test for sumArray(); uncomment it to run it
 
